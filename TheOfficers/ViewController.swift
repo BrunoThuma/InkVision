db23544c3c2e4bf1b3b951a69e6c35c432e22a79
Bruno Thuma <bruno.thuma@hotmail.com>
2021-11-01 08:55:53 -0300

    first commit

--- TheOfficers/ViewController.swift
+++ TheOfficers/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .systemCyan
         // Do any additional setup after loading the view.
     }
 
