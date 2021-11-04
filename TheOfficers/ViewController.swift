5015e8d869973bcd19a00ea6b9431f4da8587dd7
Bruno Thuma <bruno.thuma@hotmail.com>
2021-11-04 10:16:13 -0300

    fixes small view bug

--- TheOfficers/ViewController.swift
+++ TheOfficers/ViewController.swift
@@ -9,13 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var label: UILabel = .init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemCyan
         // Do any additional setup after loading the view.
+        
+        label.textColor = .black
+        label.text = "teste"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(label)
+        
+        let constraints = [
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
     }
-
-
 }
-
