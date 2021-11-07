e5ef6c50e538b213d01ce6da9774ca822b056a6d
João Pedro Picolo <picolojoaop@gmail.com>
2021-11-07 18:05:35 -0300

    Finish base plane detection

---
+++ TheOfficers/Controllers/WallDetectionViewController.swift
@@ -0,0 +1,248 @@
+//
+//  WallDetectionViewController.swift
+//  TheOfficers
+//
+//  Created by João Pedro Picolo on 04/11/21.
+//
+
+import UIKit
+import ARKit
+import SnapKit
+
+enum AppState: Int16 {
+  case lookingForWall   // Just starting out; no surfaces detected yet
+  case pointToWall      // Surfaces detected, but device is not pointing to any of them
+  case readyToPrint     // Surfaces detected *and* device is pointing to at least one
+}
+
+class WallDetectionViewController: UIViewController, ARSCNViewDelegate {
+  // MARK: - Variables
+  lazy var ARView: ARSCNView = {
+    let sceneView = ARSCNView()
+    sceneView.delegate = self
+    sceneView.automaticallyUpdatesLighting = true
+    sceneView.preferredFramesPerSecond = 60
+    sceneView.antialiasingMode = .multisampling2X
+    sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+
+    return sceneView
+  }()
+  
+  lazy var ARConfig: ARConfiguration = {
+    let config = ARWorldTrackingConfiguration()  // Use "6 degrees of freedom" tracking
+    config.worldAlignment = .gravity
+    config.planeDetection = [.vertical]
+    config.isLightEstimationEnabled = true
+    
+    return config
+  }()
+  
+  var appState: AppState = .lookingForWall
+  var statusMessage = ""
+  var trackingStatus = ""
+  
+
+  // MARK: - View initializers / events
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+      
+    if ARWorldTrackingConfiguration.isSupported {
+        ARView.session.run(ARConfig)
+    } else {
+        print("Sorry, your device doesn't support ARKit")
+    }
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    ARView.session.pause()
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    setupARView()
+    initGestureRecognizers()
+  }
+  
+  func setupARView() {
+    view.addSubview(ARView)
+    ARView.snp.makeConstraints { make in
+      make.edges.equalToSuperview()
+    }
+  }
+  
+  func resetARsession() {
+    ARView.session.run(ARConfig, options: [.resetTracking, .removeExistingAnchors])
+    appState = .lookingForWall
+  }
+  
+  // MARK: - App status
+  // This method is called once per frame, and we use it to perform tasks
+  // that we want performed constantly.
+  func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+    DispatchQueue.main.async {
+      self.updateAppState()
+      self.updateStatusText()
+    }
+  }
+
+  // Updates the app status, based on whether any of the detected planes
+  // are currently in view.
+  func updateAppState() {
+    guard appState == .pointToWall ||
+      appState == .readyToPrint
+      else {
+        return
+    }
+
+    if isAnyPlaneInView() {
+      appState = .readyToPrint
+    } else {
+      appState = .pointToWall
+    }
+  }
+
+  // Update the status text at the top of the screen whenever
+  // the AR camera tracking state changes.
+  func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+    switch camera.trackingState {
+    case .notAvailable:
+      trackingStatus = "For some reason, augmented reality tracking isn’t available."
+    case .normal:
+      trackingStatus = ""
+    case .limited(let reason):
+      switch reason {
+      case .excessiveMotion:
+        trackingStatus = "You’re moving the device around too quickly. Slow down."
+      case .insufficientFeatures:
+        trackingStatus = "I can’t get a sense of the room. Is something blocking the rear camera?"
+      case .initializing:
+        trackingStatus = "Initializing — please wait a moment..."
+      case .relocalizing:
+        trackingStatus = "Relocalizing — please wait a moment..."
+      @unknown default:
+        trackingStatus = "Error — please wait a moment..."
+      }
+    }
+  }
+
+  // Updates the status text displayed at the top of the screen.
+  func updateStatusText() {
+    switch appState {
+    case .lookingForWall:
+      statusMessage = "Scan the room with your device until the yellow dots appear."
+      ARView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+    case .pointToWall:
+      statusMessage = "Point your device towards one of the detected surfaces."
+      ARView.debugOptions = []
+    case .readyToPrint:
+      statusMessage = "Look at walls to place posters."
+      ARView.debugOptions = []
+    }
+
+//    statusLabel.text = trackingStatus != "" ? "\(trackingStatus)" : "\(statusMessage)"
+  }
+
+  // We can’t check *every* point in the view to see if it contains one of
+  // the detected planes. Instead, we assume that the planes that will be detected
+  // will intersect with at least one point on a 5*5 grid spanning the entire view.
+  func isAnyPlaneInView() -> Bool {
+    let screenDivisions = 5 - 1
+    let viewWidth = view.bounds.size.width
+    let viewHeight = view.bounds.size.height
+
+    for y in 0...screenDivisions {
+      let yCoord = CGFloat(y) / CGFloat(screenDivisions) * viewHeight
+      for x in 0...screenDivisions {
+        let xCoord = CGFloat(x) / CGFloat(screenDivisions) * viewWidth
+        let point = CGPoint(x: xCoord, y: yCoord)
+
+        // Perform hit test for planes.
+        let hitTest = ARView.hitTest(point, options: [SCNHitTestOption.searchMode : 1])
+        if !hitTest.isEmpty {
+          return true
+        }
+
+      }
+    }
+    return false
+  }
+  
+  // MARK: - Plane detection
+
+  // This delegate method gets called whenever the node for
+  // a *new* AR anchor is added to the scene.
+  func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+    guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+    let plane = Plane(anchor: planeAnchor, in: ARView)
+    
+    // Remove any children this node may have.
+    node.enumerateChildNodes { (childNode, _) in
+      childNode.removeFromParentNode()
+    }
+    node.addChildNode(plane)
+  }
+
+  // This delegate method gets called whenever the node for
+  // an *existing* AR anchor is updated.
+  func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+    // Update only anchors and nodes set up by `renderer(_:didAdd:for:)`.
+    guard let planeAnchor = anchor as? ARPlaneAnchor,
+          let plane = node.childNodes.first as? Plane
+    else { return }
+    
+    // Update extent visualization to the anchor's new bounding rectangle.
+    if let extentGeometry = plane.extentNode.geometry as? SCNPlane {
+        extentGeometry.width = CGFloat(planeAnchor.extent.x)
+        extentGeometry.height = CGFloat(planeAnchor.extent.z)
+        plane.extentNode.simdPosition = planeAnchor.center
+    }
+  }
+  
+  
+  // MARK: - Adding picture
+  // =====================================
+  func initGestureRecognizers() {
+    let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleScreenTap))
+    ARView.addGestureRecognizer(tapGestureRecognizer)
+  }
+
+  @objc func handleScreenTap(sender: UITapGestureRecognizer) {
+    let location = sender.location(in: ARView)
+    let results = ARView.hitTest(location, options: [SCNHitTestOption.searchMode : 1])
+      
+    let planeNode = results.first?.node
+      
+    if planeNode != nil {
+      // Place image here
+    }
+  }
+  
+  // MARK: - AR session error management
+
+  func session(_ session: ARSession, didFailWithError error: Error) {
+    // Present an error message to the user
+    trackingStatus = "AR session failure: \(error)"
+  }
+
+  func sessionWasInterrupted(_ session: ARSession) {
+    // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    trackingStatus = "AR session was interrupted!"
+  }
+
+  func sessionInterruptionEnded(_ session: ARSession) {
+    // Reset tracking and/or remove existing anchors if consistent tracking is required
+    trackingStatus = "AR session interruption ended."
+    resetARsession()
+  }
+}
+
+// MARK: - Utility methods
+
+// Extend the "+" operator so that it can add two SCNVector3s together.
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+  return SCNVector3(left.x + right.x,
+                    left.y + right.y,
+                    left.z + right.z)
+}
---
+++ TheOfficers/Plane.swift
@@ -0,0 +1,62 @@
+//
+//  Plane.swift
+//  TheOfficers
+//
+//  Created by João Pedro Picolo on 07/11/21.
+//
+
+import ARKit
+
+class Plane: SCNNode {
+    
+  let extentNode: SCNNode
+  var classificationNode: SCNNode?
+  
+  /// - Tag: VisualizePlane
+  init(anchor: ARPlaneAnchor, in sceneView: ARSCNView) {
+    #if targetEnvironment(simulator)
+    #error("ARKit is not supported in iOS Simulator. Connect a physical iOS device and select it as your Xcode run destination, or select Generic iOS Device as a build-only destination.")
+    #else
+    
+    // Create a node to visualize the plane's bounding rectangle.
+    let extentPlane: SCNPlane = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
+    extentNode = SCNNode(geometry: extentPlane)
+    extentNode.simdPosition = anchor.center
+    
+    // `SCNPlane` is vertically oriented in its local coordinate space, so
+    // rotate it to match the orientation of `ARPlaneAnchor`.
+    extentNode.eulerAngles.x = -.pi / 2
+
+    super.init()
+
+    self.setupExtentVisualStyle()
+    addChildNode(extentNode)
+    #endif
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setupExtentVisualStyle() {
+    // Make the extent visualization semitransparent to clearly show real-world placement.
+    extentNode.opacity = 0.7
+
+    guard let material = extentNode.geometry?.firstMaterial
+        else { fatalError("SCNPlane always has one material") }
+    
+    material.diffuse.contents = UIColor.systemBlue
+  }
+  
+  private func makeTextNode(_ text: String) -> SCNNode {
+    let textGeometry = SCNText(string: text, extrusionDepth: 1)
+    textGeometry.font = UIFont(name: "Futura", size: 75)
+
+    let textNode = SCNNode(geometry: textGeometry)
+      // scale down the size of the text
+    textNode.simdScale = SIMD3<Float>(repeating: 0.0005)
+      
+    return textNode
+  }
+}
+
